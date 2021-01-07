bd96cd23c5833c76887879f0b89e25e71675b91d
Felype Ganzert <39774570+FelypeGanzert@users.noreply.github.com>
2021-01-06 23:03:31 -0300

    POST /orders

--- backend/src/main/java/com/felypeganzert/dsdeliver/service/OrderService.java
+++ backend/src/main/java/com/felypeganzert/dsdeliver/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.felypeganzert.dsdeliver.service;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,8 +8,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.felypeganzert.dsdeliver.dto.OrderDTO;
+import com.felypeganzert.dsdeliver.dto.ProductDTO;
 import com.felypeganzert.dsdeliver.entities.Order;
+import com.felypeganzert.dsdeliver.entities.OrderStatus;
+import com.felypeganzert.dsdeliver.entities.Product;
 import com.felypeganzert.dsdeliver.repositories.OrderRepository;
+import com.felypeganzert.dsdeliver.repositories.ProductRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -18,11 +23,24 @@ public class OrderService {
 	
 	//@Autowired is not needed if we use lombok @RequiredArgsConstructor
 	private final OrderRepository repository;
+	private final ProductRepository productRepository;
 
 	@Transactional(readOnly = true)
 	public List<OrderDTO> findAll(){
 		List<Order> list = repository.findOrdersWithProducts();
 		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+	}
+	
+	@Transactional
+	public OrderDTO insert(OrderDTO dto){
+		Order order = new Order(null, dto.getAddress(),
+				dto.getLatitude(), dto.getLongitude(), Instant.now(), OrderStatus.PENDING);
+		for(ProductDTO p : dto.getProducts()) {
+			Product product = productRepository.getOne(p.getId());
+			order.getProducts().add(product);
+		}
+		order = repository.save(order);
+		return new OrderDTO(order);
 	}
 	
 }
